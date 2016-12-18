203a537c5f0ce367ab0a385556218bb057fb2135
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-12-18 20:52:13 +0100

    parseDate: fix parsing; dates < 10 start with extra space

--- src/parseDate.js
+++ src/parseDate.js
@@ -6,7 +6,7 @@ var months = [ 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'Au
  * @return {string} ISO8601 date, e.g. '2016-12-24'
  */
 function parseDate (str) {
-  var m = str.match(/^([0-9]*)\. (.*) ([0-9]{4})$/)
+  var m = str.match(/^([ 0-3][0-9])\. (.*) ([0-9]{4})$/)
 
   var r = m[3] + '-' + (months.indexOf(m[2]) + 1) + '-' + m[1]
 
