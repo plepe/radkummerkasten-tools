12b907af80d911527def215da50563f548059fe4
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-12-19 17:16:00 +0100

    Web: Correct content-type and extensions for downloads

--- src/page.js
+++ src/page.js
@@ -5,13 +5,24 @@ window.createCsv = require('../src/createCsv')
 window.createGeoJson = require('../src/createGeoJson')
 window.stream = require('stream')
 
-function createDownload (data) {
+function createDownload (fileType, data) {
   var download = document.getElementById('download')
   download.innerHTML = ''
 
+  var contentType
+  var extension
+
+  if (fileType === 'csv') {
+    contentType = 'text/csv'
+    extension = 'csv'
+  } else if (fileType === 'geojson') {
+    contentType = 'application/vnd.geo+json'
+    extension = 'geojson'
+  }
+
   var a = document.createElement('a')
-  a.href= 'data:text/csv;charset=utf-8,' + encodeURI(data)
-  a.download = 'radkummerkasten.csv'
+  a.href= 'data:' + contentType + ';charset=utf-8,' + encodeURI(data)
+  a.download = 'radkummerkasten.' + extension
   a.appendChild(document.createTextNode('Download'))
 
   download.appendChild(a)
@@ -34,10 +45,11 @@ window.submitRequest = function () {
   var download = document.getElementById('download')
   download.innerHTML = 'Daten werden geladen, bitte warten ...'
 
-  if (form.elements.fileType.value === 'csv') {
-    createCsv(filter, concat(createDownload))
-  } else if (form.elements.fileType.value === 'geojson') {
-    var downloadStream = concat(createDownload)
+  var fileType = form.elements.fileType.value
+  if (fileType === 'csv') {
+    createCsv(filter, concat(createDownload.bind(this, fileType)))
+  } else if (fileType === 'geojson') {
+    var downloadStream = concat(createDownload.bind(this, fileType))
 
     createGeoJson(filter, downloadStream, function () {
       downloadStream.end()
