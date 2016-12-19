2bd7bd0990a4d6f1a1d4fde4a64790804812b87f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-12-19 13:19:33 +0100

    Compile dist file

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var Radkummerkasten = require('./src/Radkummerkasten')
-
-Radkummerkasten.getEntries(
-  {
-  },
-  function (err, entry) {
-    if (err) {
-      throw (err)
-    }
-    console.log(JSON.stringify(entry))
-  },
-  function (err) {
-    if (err) {
-      throw (err)
-    }
-  }
-)
+window.Radkummerkasten = require('./src/Radkummerkasten')
