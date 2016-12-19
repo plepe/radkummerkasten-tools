85f826c621e8068bca8b7667f6c38024857656ec
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-12-19 13:20:02 +0100

    rkk-csv: move code to src/createCsv.js

---
+++ src/createCsv.js
@@ -0,0 +1,32 @@
+var csvWriter = require('csv-write-stream')
+var Radkummerkasten = require('./Radkummerkasten')
+
+module.exports = function (filter, pipe) {
+  var headers = [ 'id', 'bezirk', 'category', 'categoryName', 'status', 'lat', 'lon' ]
+  if (filter.includeDetails) {
+    headers = [ 'id', 'title', 'bezirk', 'user', 'date', 'category', 'categoryName', 'status', 'likes', 'lat', 'lon', 'text', 'commentsCount', 'attachmentsCount' ]
+  }
+
+  var writer = csvWriter({
+    headers: headers
+  })
+  writer.pipe(pipe)
+
+  Radkummerkasten.getEntries(
+    filter,
+    function (err, entry) {
+      if (err) {
+        throw (err)
+      }
+
+      writer.write(entry)
+    },
+    function (err) {
+      if (err) {
+        throw (err)
+      }
+
+      writer.end()
+    }
+  )
+}
