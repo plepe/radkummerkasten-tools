c0ed0abf1a467d5926f902bb0106063c3fee5b83
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-12-19 10:42:57 +0100

    CLI: Improve argument handling

--- src/cliHandleArguments.js
+++ src/cliHandleArguments.js
@@ -1,47 +1,60 @@
-var ArgumentParser = require('argparse').ArgumentParser
+var parser
 
-var parser = new ArgumentParser({
-  description: 'Lade Einträge aus dem Radkummerkasten und generiere eine CSV Datei daraus.',
-  addHelp: true,
-  epilog: 'Die radkummerkasten-tools werden von Stephan Bösch-Plepelits entwickelt. Diese sind Open Source und werden auf Github entwickelt. Kommentare, Bug Reports und Erweiterungen sind herzlich willkommen: https://github.com/plepe/radkummerkasten-tools'
-})
+function init () {
+  var ArgumentParser = require('argparse').ArgumentParser
 
-parser.addArgument(
-  [ '-d', '--details' ],
-  {
-    help: 'Lade außerdem die detailierten Daten für alle (passenden) Einträge (Achtung: Für jeden Eintrag muss eine Anfrage an den Server gestellt werden - das dauert).',
-    nargs: 0
-  }
-)
+  parser = new ArgumentParser({
+    description: 'Lade Einträge aus dem Radkummerkasten und generiere eine CSV Datei daraus.',
+    addHelp: true,
+    epilog: 'Die radkummerkasten-tools werden von Stephan Bösch-Plepelits entwickelt. Diese sind Open Source und werden auf Github entwickelt. Kommentare, Bug Reports und Erweiterungen sind herzlich willkommen: https://github.com/plepe/radkummerkasten-tools'
+  })
 
-parser.addArgument(
-  [ 'id' ],
-  {
-    help: 'Lade nur Einträge mit den angegebenen IDs (siehe Nummer in der URL eines Eintrages: z.B.: "#marker-1234" => "1234").',
-    nargs: '*'
-  }
-)
+  parser.addArgument(
+    [ '-d', '--details' ],
+    {
+      help: 'Lade außerdem die detailierten Daten für alle (passenden) Einträge (Achtung: Für jeden Eintrag muss eine Anfrage an den Server gestellt werden - das dauert).',
+      nargs: 0
+    }
+  )
 
-parser.addArgument(
-  [ '-b', '--bezirk' ],
-  {
-    help: 'Lade nur Einträge im angegebenen Bezirk, bzw. in den angegebenen Bezirken.',
-    nargs: '*'
-  }
-)
+  parser.addArgument(
+    [ 'id' ],
+    {
+      help: 'Lade nur Einträge mit den angegebenen IDs (siehe Nummer in der URL eines Eintrages: z.B.: "#marker-1234" => "1234").',
+      nargs: '*'
+    }
+  )
 
-parser.addArgument(
-  [ '-k', '--kategorie' ],
-  {
-    help: 'Lade nur Einträge der angegeben Kategorie(n). Diese können als nummerischer Wert oder Text angegeben werden.',
-    nargs: '*',
-    dest: 'category'
-  }
-)
+  parser.addArgument(
+    [ '-b', '--bezirk' ],
+    {
+      help: 'Lade nur Einträge im angegebenen Bezirk, bzw. in den angegebenen Bezirken.',
+      nargs: '*'
+    }
+  )
 
-var args = parser.parseArgs()
-if (args.id.length === 0) {
-  delete args.id
+  parser.addArgument(
+    [ '-k', '--kategorie' ],
+    {
+      help: 'Lade nur Einträge der angegeben Kategorie(n). Diese können als nummerischer Wert oder Text angegeben werden.',
+      nargs: '*',
+      dest: 'category'
+    }
+  )
+
+  return parser
 }
 
-module.exports = args
+function parseArgs () {
+  var args = parser.parseArgs()
+  if (args.id.length === 0) {
+    delete args.id
+  }
+
+  return args
+}
+
+module.exports = {
+  init: init,
+  parseArgs: parseArgs
+}
