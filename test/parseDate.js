9c8568a765d075f9e5b3afdd9c44a444f791be41
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-12-23 22:17:45 +0100

    Tests for parseDate() (failing)

---
+++ test/parseDate.js
@@ -0,0 +1,18 @@
+var parseDate = require('../src/parseDate')
+var assert = require('assert')
+
+describe('parseDate', function () {
+  var dates = [
+    [ '24. Dezember 2016', '2016-12-24' ],
+    [ ' 8. März 2016', '2016-03-08' ],
+    [ ' 8. Dezember 2016', '2016-12-08' ]
+  ]
+
+  it('parse dates', function () {
+    for (var i = 0; i < dates.length; i++) {
+      assert.equal(parseDate(dates[i][0]), dates[i][1], dates[i][0] + ' should parse to ' + dates[i][1])
+    }
+
+    return true
+  })
+})
