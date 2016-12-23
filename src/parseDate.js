00664a6aa990a3c538b7bcf23d8bdbea8265592a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-12-23 22:17:55 +0100

    parseDate(): bugfix

--- src/parseDate.js
+++ src/parseDate.js
@@ -1,5 +1,11 @@
 var months = [ 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember' ]
 
+function zeroPad (n) {
+  n = '' + parseInt(n)
+
+  return '00'.substr(0, 2 - n.length) + n
+}
+
 /**
  * parse ISO8601 date from German date string
  * @param {string} str - German date string, e.g. '24. Dezember 2016'
@@ -8,7 +14,7 @@ var months = [ 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'Au
 function parseDate (str) {
   var m = str.match(/^([ 0-3][0-9])\. (.*) ([0-9]{4})$/)
 
-  var r = m[3] + '-' + (months.indexOf(m[2]) + 1) + '-' + m[1]
+  var r = m[3] + '-' + zeroPad(months.indexOf(m[2]) + 1) + '-' + zeroPad(m[1])
 
   return r
 }
