dce43c12fc9fd6bd3f0efa1c1c762e3bff1116be
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-02-01 21:28:57 +0100

    toDataUrl(): NodeJS compatibility
    - use module w3c-xmlhttprequest instead of xmlhttprequest
    - workaround as response is ArrayBuffer

--- src/to-data-url.js
+++ src/to-data-url.js
@@ -3,11 +3,22 @@ function toDataUrl(url, callback) {
   var xhr = new XMLHttpRequest()
   xhr.responseType = 'blob'
   xhr.onload = function() {
-    var reader = new FileReader()
-    reader.onloadend = function() {
-      callback(null, reader.result)
+    if (xhr.response.constructor.name === 'ArrayBuffer') {
+      // the NodeJS module 'w3c-xmlhttprequest' returns an ArrayBuffer if
+      // repsonseType is set to 'blob'. Use the following code as alternative
+      // to the "correct" version below.
+      var b64 = new Buffer(xhr.response).toString('base64')
+      var contentType = xhr.getResponseHeader('content-type')
+      callback(null, 'data:' + contentType + ';base64,' + b64)
+
+    } else {
+      // "correct" version
+      var reader = new FileReader()
+      reader.onloadend = function() {
+        callback(null, reader.result)
+      }
+      reader.readAsDataURL(xhr.response)
     }
-    reader.readAsDataURL(xhr.response)
   }
   xhr.open('GET', url)
   xhr.send()
