3380934b430879e2eeecd9a4df1ed10bd7db4672
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-03-04 19:24:40 +0100

    Use modulekit-auth for authentication

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php include "conf.php"; /* load a local configuration */ ?>
 <?php include "modulekit/loader.php"; /* loads all php-includes */ ?>
+<?php session_start(); ?>
 <?php call_hooks("init"); /* initialize submodules */ ?>
+<?php
+$auth = new Auth();
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
--- modulekit.php
+++ modulekit.php
@@ -1,7 +1,7 @@
 <?php
 $id = "radkummerkasten-tools";
 $name = "radkummerkasten-tools";
-$depend = array("modulekit-form", "PDOext"); // use modulekit-form and all its requirements
+$depend = array("modulekit-form", "PDOext", "modulekit-auth"); // use modulekit-form and all its requirements
 $include = array(
 );
 $version = "0.6.3";
