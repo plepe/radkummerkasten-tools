b584761ef5b1c1f26a259f7617d36b74a80a899a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-03-15 10:35:14 +0100

    inlineForms: modify data in subtables

--- src/inlineForms.js
+++ src/inlineForms.js
@@ -24,12 +24,30 @@ function inlineForms2 (div, entry, emitter, err, def) {
     }
 
     if (span.className == 'inlineForm') {
+      var pathDef = def
       var fieldId = span.getAttribute('field')
-      if (!(fieldId in def)) {
-        return
+
+      var path = span.getAttribute('path')
+      if (path) {
+        path.split('/').forEach(p => {
+          let pp = p.split('.')
+          if (pp[0] in pathDef && 'def' in pathDef[pp[0]] && 'def' in pathDef[pp[0]].def) {
+            pathDef = pathDef[pp[0]].def.def
+          } else {
+            pathDef = null
+          }
+        })
+
+        if (pathDef === null) {
+          return
+        }
       }
 
-      var fieldDef = def[fieldId]
+      if (!(fieldId in pathDef)) {
+        return
+      }
+      var fieldDef = pathDef[fieldId]
+
       if (!fieldDef.may_write) {
         return
       }
@@ -38,13 +56,26 @@ function inlineForms2 (div, entry, emitter, err, def) {
         span.removeChild(span.firstChild)
       }
 
-      var f = new form('inline-' + entry.id + '-' + fieldId, {}, fieldDef)
+      var formId = 'inline-' + entry.id + '-' + path + '-' + fieldId
+      var f = new form(formId, {}, fieldDef)
       f.set_data(span.getAttribute('value'))
       f.show(span)
 
       f.onchange = function () {
         var data = {}
-        data[fieldId] = f.get_data()
+        var d = data
+
+        if (path) {
+          path.split('/').forEach(p => {
+            let pp = p.split('.')
+            d[pp[0]] = [{
+              id: pp[1]
+            }]
+            d = d[pp[0]][0]
+          })
+        }
+
+        d[fieldId] = f.get_data()
 
         entry.save(data,
           function (err) {
