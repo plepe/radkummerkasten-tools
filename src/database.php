78124ba5bd7359bf9852a543b349261ed6900256
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-03-25 16:51:09 +0200

    Database: implement rights

--- src/database.php
+++ src/database.php
@@ -12,7 +12,11 @@ register_hook('init', function () {
 
   $anonym = true;
 
-  if ($auth->current_user->is_logged_in()) {
+  $is_logged_in = $auth->is_logged_in();
+  $is_admin = $auth->access('administrator');
+  $is_editor = $is_admin || $auth->access('editor'); // every admin is editor
+
+  if ($is_editor || $is_admin) {
     $anonym = false;
   }
 
@@ -25,27 +29,35 @@ register_hook('init', function () {
       ),
       'lat' => array(
         'type' => 'float',
+        'write' => $is_admin,
       ),
       'lng' => array(
         'type' => 'float',
+        'write' => $is_admin,
       ),
       'survey' => array(
         'type' => 'int',
+        'write' => $is_editor,
       ),
       'street' => array(
         'type' => 'text',
+        'write' => $is_admin,
       ),
       'housenumber' => array(
         'type' => 'text',
+        'write' => $is_admin,
       ),
       'postcode' => array(
         'type' => 'int',
+        'write' => $is_admin,
       ),
       'city' => array(
         'type' => 'text',
+        'write' => $is_admin,
       ),
       'date' => array(
         'type' => 'text',
+        'write' => $is_admin,
       ),
       'day' => array(
         'type' => 'text',
@@ -65,17 +77,21 @@ register_hook('init', function () {
           ),
           'marker' => array(
             'type' => 'int',
+            'write' => $is_admin,
           ),
           'message' => array(
             'type' => 'text',
+            'write' => $is_admin,
           ),
           'firstname' => array(
             'type' => 'text',
             'read' => true,
+            'write' => $is_admin,
           ),
           'lastname' => array(
             'type' => 'text',
             'select' => $anonym ? "concat(substr(name, 1, 1), '.')" : "name",
+            'write' => $is_admin,
           ),
           'name' => array(
             'type' => 'text',
@@ -85,16 +101,19 @@ register_hook('init', function () {
           'email' => array(
             'type' => 'text',
             'read' => !$anonym,
+            'write' => $is_admin,
           ),
           'gender' => array(
             'type' => 'int',
             'read' => !$anonym,
+            'write' => $is_admin,
           ),
           'newsletter' => array(
             'type' => 'int',
           ),
           'date' => array(
             'type' => 'text',
+            'write' => $is_admin,
           ),
           'day' => array(
             'type' => 'text',
@@ -106,6 +125,7 @@ register_hook('init', function () {
           ),
           'visible' => array(
             'type' => 'boolean',
+            'write' => $is_editor,
           ),
           'attachments' => array(
             'type' => 'sub_table',
@@ -117,6 +137,7 @@ register_hook('init', function () {
               ),
               'context' => array(
                 'type' => 'int',
+                'write' => $is_admin,
               ),
               'type' => array(
                 'type' => 'int',
@@ -161,13 +182,16 @@ register_hook('init', function () {
       ),
       'visible' => array(
         'type' => 'boolean',
+        'write' => $is_editor,
       ),
       'status' => array(
         'type' => 'int',
         'write' => true,
+        'write' => $is_editor,
       ),
       'address' => array(
         'type' => 'text',
+        'write' => $is_admin,
       ),
     ),
   );
