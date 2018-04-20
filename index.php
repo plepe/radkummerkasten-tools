601c3d93511f4ab6127287146271ecddda9840bd
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-04-20 22:05:32 +0200

    Use composer for additional dependencies

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include "conf.php"; /* load a local configuration */ ?>
+<?php include "vendor/autoload.php"; ?>
 <?php include "modulekit/loader.php"; /* loads all php-includes */ ?>
 <?php session_start(); ?>
 <?php call_hooks("init"); /* initialize submodules */ ?>
